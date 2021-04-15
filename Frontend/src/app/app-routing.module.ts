b19f05dc6c949a63517ffd84d76711843a787a94
Andrea Bullos <54980279+ABullos@users.noreply.github.com>
2021-04-15 21:40:30 +0800

    fixed nav bar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent} from './components/contact/contact.component';
 import { ServicesComponent } from './components/services/services.component';
-import { NavbarComponent } from './components/navbar/navbar.component'
 import { ProductsComponent} from './components/products/products.component'
 
 
@@ -14,7 +13,6 @@ const routes: Routes = [
 { path: 'about', component: AboutComponent},
 { path: 'contact', component: ContactComponent},
 { path: 'services', component: ServicesComponent},
-{ path: 'navbar', component: NavbarComponent} ,
 { path: 'products', component: ProductsComponent}
 
 ];
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
-    ProductsComponent
+  routingComponents
   ],
   imports: [
     BrowserModule,
