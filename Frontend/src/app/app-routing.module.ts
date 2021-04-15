db65b32399c59d6f29a6a9b944372f33743257c1
Andrea Bullos <54980279+ABullos@users.noreply.github.com>
2021-04-15 17:40:41 +0800

    routing components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent} from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
-const routes: Routes = [];
 
+
+const routes: Routes = [
+{ path: 'home', component: HomeComponent},
+{ path: 'about', component: AboutComponent},
+{ path: 'contact', component: ContactComponent},
+{ path: 'services', component: ServicesComponent},
+{ path: 'navbar', component: NavbarComponent} 
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, AboutComponent, ContactComponent, ServicesComponent]
