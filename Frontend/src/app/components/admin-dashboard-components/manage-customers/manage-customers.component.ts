ab1bea68959914ee82746d7bbeda70dd4d93e0be
rutadeline <uwibambe_adeline@yahoo.com>
2021-05-02 17:13:00 +0800

    Updates on manage-customers component

--- Frontend/src/app/components/admin-dashboard-components/manage-customers/manage-customers.component.ts
+++ Frontend/src/app/components/admin-dashboard-components/manage-customers/manage-customers.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Customers } from 'src/app/shared/customers.model';
 import { CustomersService } from 'src/app/shared/customers.service';
 import { UserService } from 'src/app/shared/user.service';
-
 
 @Component({
   selector: 'app-manage-customers',
   templateUrl: './manage-customers.component.html',
-  styleUrls: ['./manage-customers.component.css']
+  styleUrls: ['./manage-customers.component.css'],
+  providers: [CustomersService]
 })
 export class ManageCustomersComponent implements OnInit {
   showHideCustomersForm = false;
+  customers;
   userDetails;
 
   constructor(
     private userService: UserService,
     private router: Router,
-    public customerService: CustomersService
+    public customersService: CustomersService
   ) { }
 
   ngOnInit(): void {
@@ -26,25 +29,79 @@ export class ManageCustomersComponent implements OnInit {
       }
     );
 
-    this.showHideCustomersForm = true;
+    this.resetCustomersForm();
+    this.refreshCustomersList();
+
   }
 
   toggleDisplayDivIf() {
     this.showHideCustomersForm = !this.showHideCustomersForm;
   }
 
-  //edit or update customer info
-  onEditCustomer() {
-    //edit customer code here
+  showCustomers() {
+    this.customersService.getCustomer()
+    .subscribe((data: any) => this.customers = data
+    );
+    console.log(this.customers);
+  }
 
+  //edit or update customer info
+  onEditCustomer(service : Customers) {
+    this.customersService.selectedService = service;
     this.showHideCustomersForm = false;
   }
 
-  //reset edit service form (this is the method for the cancel button on the form)
-  resetCustomersForm() {
-    //resetCustomersForm code here
+  //refresh Customers list
+  refreshCustomersList() {
+    this.customersService.getCustomer().subscribe((res) => {
+      this.customersService.customersList = res as Customers[];
+    });
+  }
 
+  //delete Customer
+  onDeleteCustomer(_id: string){
+    if(confirm('Are you sure you want to delete this customer?') == true) {
+      this.customersService.deleteCustomer(_id).subscribe((res) => {
+        this.refreshCustomersList();
+        alert('Customer Deleted Successfully');
+      });
+    }
+  }
+
+  onSubmitCustomer(form : NgForm){
+    //add new customer
+    if(form.value._id == "") {
+      this.customersService.postCustomer(form.value).subscribe((res) => {
+        this.resetCustomersForm(form);
+        this.refreshCustomersList();
+        alert('New Customer added Successfully!');
+      });
+
+    //update existing customer
+    } else {
+      this.customersService.editCustomer(form.value).subscribe((res) => {
+        this.resetCustomersForm(form);
+        this.refreshCustomersList();
+        alert('Customer Information Updated Successfully');
+      });
+    }
+  }
+
+  //reset edit service form (this is the method for the cancel button on the form)
+  resetCustomersForm(form ?: NgForm) {
+    if (form)
+      form.reset();
+    this.customersService.selectedService = {
+      _id: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      address: "",
+      phoneNumber: "",
+    }
     this.showHideCustomersForm = true;
+    this.refreshCustomersList();
+
   }
 
   onLogout() {
