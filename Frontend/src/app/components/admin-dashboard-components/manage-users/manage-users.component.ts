28a7c4a83fce2eef7917f93ab0d41bdcf75bfe73
rutadeline <uwibambe_adeline@yahoo.com>
2021-05-02 18:03:13 +0800

    Update manage users.ts

--- Frontend/src/app/components/admin-dashboard-components/manage-users/manage-users.component.ts
+++ Frontend/src/app/components/admin-dashboard-components/manage-users/manage-users.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
-  styleUrls: ['./manage-users.component.css']
+  styleUrls: ['./manage-users.component.css'],
+  providers: [UserService]
 })
+
 export class ManageUsersComponent implements OnInit {
   showHideUsersForm = false;
+  users;
   userDetails;
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    public usersService: UserService
   ) { }
 
   ngOnInit(): void {
@@ -23,10 +28,20 @@ export class ManageUsersComponent implements OnInit {
         this.userDetails = res['user'];
       }
     );
+
+    this.resetUsersForm();
+    this.refreshUsersList();
   }
 
   toggleDisplayDivIf() {
     this.showHideUsersForm = !this.showHideUsersForm;
+  }
+
+  ShowUsers() {
+    this.usersService.getUsers()
+    .subscribe((data: any) => this.users = data
+    );
+    console.log(this.users);
   }
 
   //edit or update customer info
@@ -36,11 +51,41 @@ export class ManageUsersComponent implements OnInit {
     this.showHideUsersForm = false;
   }
 
+  refreshUsersList(){
+
+  }
+
   //reset edit service form (this is the method for the cancel button on the form)
-  resetUsersForm() {
-    //resetCustomersForm code here
+  resetUsersForm(form ?: NgForm) {
+    if (form)
+      form.reset();
+    this.usersService.selectedUser ={
+      _id: "",
+      fname: "",
+      lname: "",
+      username: "",
+      email: "",
+      address: "",
+      phoneNumber: "",
+      password: "",
+      role: "",
+    }
+
 
     this.showHideUsersForm = true;
+  }
+
+  onSubmitUser(){
+
+  }
+
+  onDeleteUser(_id: string){
+    if(this.userDetails('Are you sure you want to delete this User?') == true) {
+      this.usersService.deleteUser(_id).subscribe((res) => {
+        this.refreshUsersList();
+      })
+    }
+
   }
 
   //user logout
